64e6d183bf98384344ae84f32b8f91b3f9a4461f
conejox <albertoconejerosa@gmail.com>
2025-04-24 10:36:41 +0200

    image

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,49 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css"; // Import the CSS file
 
 function App() {
   const [projects, setProjects] = useState([]);
 
   useEffect(() => {
-    axios.get('http://localhost:5000/api/projects')
-      .then(response => setProjects(response.data))
-      .catch(error => console.error("Error:", error));
+    axios
+      .get("http://localhost:5000/api/projects")
+      .then((response) => setProjects(response.data))
+      .catch((error) => console.error("Error:", error));
   }, []);
 
   return (
-    <div>
+    <div className="app-container">
+      <div className="profile-section">
+      <img
+          src="/images/perfilfoto.jpg"
+          alt="Profilefoto"
+          className="profile-image"
+        />
+        <h1>Alberto Conejeros</h1>
+        <p>junior developer</p>
+      </div>
+
+      <div className="link-icons">
+        <a href="YOUR_LINKEDIN_URL" target="_blank" rel="noopener noreferrer">
+          <i className="fab fa-linkedin"></i> {/* LinkedIn Icon */}
+        </a>
+        <a href="YOUR_GITHUB_URL" target="_blank" rel="noopener noreferrer">
+          <i className="fab fa-github"></i> {/* GitHub Icon */}
+        </a>
+        <a href="YOUR_TWITTER_URL" target="_blank" rel="noopener noreferrer">
+          <i className="fab fa-twitter"></i> {/* Twitter Icon */}
+        </a>
+        <a href="YOUR_PERSONAL_URL" target="_blank" rel="noopener noreferrer">
+          <i className="fas fa-home"></i> {/* Personal Website Icon */}
+        </a>
+        <a href="YOUR_EMAIL_URL">
+          <i className="fas fa-envelope"></i> {/* Email Icon */}
+        </a>
+      </div>
       <h1>Mi Portafolio hola</h1>
       <ul>
-        {projects.map(project => (
+        {projects.map((project) => (
           <li key={project.id}>
             {project.name} - {project.tech}
           </li>
